f786681d84b8e15912e0e3400dfd4f48e0210e13
oh young seok <oyg9731@naver.com>
2023-07-24 15:28:18 +0900

    js/core/chap01 변수와상수. 데이터타입, 산술연산자 학습

---
+++ app.js
@@ -0,0 +1,13 @@
+// alert('메롱메롱');
+
+// const a = 10, b= 20;
+// console.log(a * b);
+
+const a = 10
+const b = 10
+console.log(a+b)
+
+function foo(){
+    return{age: 10, nick: '바보'};
+}
+console.log(foo());
