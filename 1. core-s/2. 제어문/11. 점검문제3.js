09c7f7e0087b29b647e2e8ec5d39e521013053c5
oh young seok <oyg9731@naver.com>
2023-07-26 10:23:50 +0900

    사칙연산 게임 세번째버전 - 뺄셈, 곱셈 추가

--- 1. core-s/2. 제어문/11. 점검문제3.js
+++ 1. core-s/2. 제어문/11. 점검문제3.js
@@ -186,34 +186,48 @@
 // }
 
 // =================================================================
-
-let qNum = 1;
+let qNum = 1; // 문제 넘버링
 
 // 정답, 오답 횟수
 let correctCount = 0;
 let wrongCount = 0;
 
-while(true){
-    // 랜덤 정수 2개를생성
-    let firstNumber = Math.floor(Math.random() * 20) + 1;
-    let secondNumber = Math.floor(Math.random() * 20) + 1;
+while (true) {
+  // 랜덤 정수 2개를 생성
+  let firstNumber = Math.floor(Math.random() * 20) + 1;
+  let secondNumber = Math.floor(Math.random() * 20) + 1;
 
-    let userAnswer = +prompt(`Q${qNum++}. ${firstNumber} + ${secondNumber} = ??`);
+  // 부호를 만들 랜덤 정수 생성
+  let markNum = Math.floor(Math.random() * 3);
 
-    if(userAnswer === 0){
-        alert(`게임을 종료합니다!!`);
-        break;
-    }
-}
+  let mark;
+  let realAnswer; // 실제 정답
+  if (markNum === 0) {
+    mark = '+';
+    realAnswer = firstNumber + secondNumber;
+  } else if (markNum === 1) {
+    mark = '-';
+    realAnswer = firstNumber - secondNumber;
+  } else {
+    mark = 'x';
+    realAnswer = firstNumber * secondNumber;
+  }
+  
+  let userAnswer = +prompt(`Q${qNum++}. ${firstNumber} ${mark} ${secondNumber} = ??`);
 
+  if (userAnswer === 0) {
+    alert('게임을 종료합니다!');
+    break;
+  }
 
-// 정답확인
-if(userAnswer === firstNumber + secondNumber){
-    alert(`정답입니다!`);
+  // 정답 확인
+  if (userAnswer === realAnswer) {
+    alert("정답입니다!");
     correctCount++;
-}else{
-    alert(`틀렸습니다ㅠㅠ`)
+  } else {
+    alert("틀렸습니다!");
     wrongCount++;
+  }
 } // end while
 
-alert(`정답횟수 : ${correctCount}회, 틀린횟수 : ${wrongCount}회`);
+alert(`정답 횟수: ${correctCount}회, 틀린 횟수: ${wrongCount}회`);
