a801d5b1cc36cfaf1f058c5712aa6f4e7d66468d
oh young seok <oyg9731@naver.com>
2023-08-04 10:11:20 +0900

    web api - 실전예제2: 영화 삭제 모달 구현
    - 삭제 모달 구현
    - 조건부 섹션 렌더링

--- 2. web-api/App/Movie/assets/scripts/app.js
+++ 2. web-api/App/Movie/assets/scripts/app.js
@@ -2,6 +2,16 @@
 const $addMovieModal = document.getElementById("add-modal");
 const $deleteMovieModal = document.getElementById('delete-modal');
 
+// 삭제창 버튼
+const $deleteButton = document.querySelector('.btn--danger');
+const $cancleDeleteButton = document.querySelector('#delete-modal .btn--passive');
+let a = 0;
+let movieId = 0;
+let index = 0;
+
+// 카드 지정
+const $card = document.getElementById('entry-text');
+
 // 영화 추가 버튼
 const $addMovieButton = document.querySelector("header button");
 const $backdrop = document.getElementById("backdrop");
@@ -39,6 +49,25 @@ const closeAddModal = () => {
   clearMovieModalInput();
 };
 
+// 영화삭제모달을 닫는 함수 @@@@@@@@@@@@@@@@@@@@
+const closedeleteModal = () => {
+  $backdrop.classList.remove(CLASS_VISIBLE);
+  $deleteMovieModal.classList.remove(CLASS_VISIBLE);
+};
+
+// card 삭제
+const cardVisible = () => {
+    
+     if(movies.length >= 1){
+        $card.classList.add(CLASS_VISIBLE);
+        
+     }else{
+        $card.classList.remove(CLASS_VISIBLE);
+        console.log('확인이용');
+     }
+};
+
+
 // 화면에 새로운 영화 정보를 렌더링하는 함수
 const renderNewMovie = ({ id, title, image, rating }) => {
   const $newMovie = document.createElement('li');
@@ -55,12 +84,13 @@ const renderNewMovie = ({ id, title, image, rating }) => {
     </div>
   `;
 
+
   // 삭제를 진행하는 핸들러
   const deleteMovieHandler = e => {
 
     // 배열에서도 영화 정보를 지워야 함!
     // 클릭한 태그의 근처 li의 movie-id값 가져오기
-    const movieId = e.target.closest('.movie-element').dataset.movieId;
+    movieId = e.target.closest('.movie-element').dataset.movieId;
     console.log(movieId);
 
     // 배열에서 해당 아이디값을 가지는 객체를 찾아내고 인덱스를 알아내기
@@ -75,20 +105,24 @@ const renderNewMovie = ({ id, title, image, rating }) => {
     // 대상의 인덱스 찾기
     // indexOf: 원시타입 (숫자, 문자열)만 찾을 수 있음
     // findIndex: 배열 고차함수 
-    const index = movies.findIndex(m => m.id === movieId);
+    index = movies.findIndex(m => m.id === movieId);
     console.log(`클릭대상 인덱스: ${index}`);
 
     // 그 객체를 배열에서 지우기 <- ?? : 인덱스를 알아야 지우지
-    movies.splice(index, 1);
+    // movies.splice(index, 1);
     
     // 실제 li 지우기
-    e.target.closest('.movie-element').remove();
+    a = e.target.closest('.movie-element');
+    deleteMovieModalHandler();
+    
   };
 
   // 삭제 클릭 이벤트
-  $newMovie.addEventListener('click', deleteMovieHandler)
+  $newMovie.addEventListener('click', deleteMovieHandler);
+//   $deleteButton.addEventListener('click', deleteMovieHandler);
 
   $movieList.appendChild($newMovie);
+  cardVisible();
 };
 
 // 영화 정보 입력란 검증 
@@ -106,6 +140,14 @@ const validateMovieInput = ({title, image, rating}) => {
 
 // =====  이벤트 핸들러 및 이벤트 바인딩 ===== //
 
+// 삭제창 yes버튼을 눌렀을때
+const delete1 = () =>
+{
+    closedeleteModal();
+    movies.splice(index, 1);
+    a.remove();
+    cardVisible();
+}
 // 영화 추가버튼 기능을 수행하는 핸들러
 const addMovieHandler = e => {
   const titleValue = $titleInput.value; // 제목입력란값
@@ -142,14 +184,24 @@ const showMovieModalHandler = e => {
   $backdrop.classList.add(CLASS_VISIBLE);
 };
 
-
+// 영화 삭제 모달창 띄우는 핸들러 @@@@@@@@@@
+const deleteMovieModalHandler = e => {
+    $deleteMovieModal.classList.add(CLASS_VISIBLE);
+    $backdrop.classList.add(CLASS_VISIBLE);
+  };
+  
 
 // 백드롭 영역을 클릭하면 모달이 닫히는 핸들러
 const backdropHandler = e => {
   closeAddModal();
+  closedeleteModal();
 };
 // 영화 추가 모달창의 취소버튼을 누르면 모달이 닫히는 핸들러
 const closeMovieModalHandler = e => {
+  closeAddModal();
+};
+// 영화 추가 모달창의 취소버튼을 누르면 모달이 닫히는 핸들러
+const deleteCloseMovieModalHandler = e => {
   closeAddModal();
 };
 
@@ -164,3 +216,9 @@ $cancelAddMovieButton.addEventListener('click', closeMovieModalHandler);
 
 // Add Movie모달 추가버튼 클릭이벤트
 $confirmAddMovieButton.addEventListener('click', addMovieHandler);
+
+// delete movie 모달 삭제버튼 클릭 이벤트 @@@@@@@@@@@@
+$deleteButton.addEventListener('click', delete1);
+
+// delete movie 모달 삭제 취소 버튼 클릭 이벤트 @@@@@@@@@@
+$cancleDeleteButton.addEventListener('click', closedeleteModal);
